2979b086872e0a85e5a539eee9b87e08ec4731ab
Zorgit <piligrimexe@Gmail.com>
2022-09-14 00:34:10 +0300

    Add cod for DrumKit class and DrumKitTestDrive class

--- Part2/src/P73/DrumKit.java
+++ Part2/src/P73/DrumKit.java
@@ -1,4 +1,34 @@
 package P73;
 
-public class DrumKit {
+class DrumKit {
+    boolean topHat = false;
+    boolean snare = false;
+
+    void playSnare() {
+        System.out.println("бах бах ба-бах");
+    }
+
+    void playTopHat() {
+        System.out.println("динь динь ди-динь");
+    }
 }
+
+class DrumKitTestDrive {
+    public static void main (String [] args ) {
+        DrumKit d = new DrumKit();
+        d.snare=true;
+        d.topHat=true;
+
+        if (d.snare == true) {
+            d.playSnare();
+        }
+
+        if (d.topHat == true) {
+            d.playTopHat();
+        }
+
+    }
+}
+
+
+
