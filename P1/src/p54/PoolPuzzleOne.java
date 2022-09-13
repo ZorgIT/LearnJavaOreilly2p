3a1e51c7964cf070c97daf998499dc43a753b7ff
Zorgit <piligrimexe@Gmail.com>
2022-09-13 21:42:09 +0300

    Add basic structure in PoolPuzzleOne.java

--- P1/src/p54/PoolPuzzleOne.java
+++ P1/src/p54/PoolPuzzleOne.java
@@ -1,4 +1,34 @@
 package p54;
 
 public class PoolPuzzleOne {
+    public static void main (String [] args) {
+        int x = 0;
+
+        while () {
+            __________
+            if (x < 1) {
+                __________
+            }
+            __________
+            if () {
+                __________
+                __________
+            }
+
+
+            if ( == 1) {
+                __________
+            }
+            if () {
+                __________
+            }
+            System.out.println ("");
+            __________
+        }
+
+    }
 }
+
+
+
+
