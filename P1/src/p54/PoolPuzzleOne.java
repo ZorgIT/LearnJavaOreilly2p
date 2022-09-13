f716cf12d632196551d0d8a95ce223f76ba61479
Zorgit <piligrimexe@Gmail.com>
2022-09-13 21:42:50 +0300

    Add "pool fragment" in PoolPuzzleOne.java

--- P1/src/p54/PoolPuzzleOne.java
+++ P1/src/p54/PoolPuzzleOne.java
@@ -29,6 +29,28 @@ public class PoolPuzzleOne {
     }
 }
 
+/*
+add some pool fragment
+System.out.print(" ");
+System.out.print("a ");
+System.out.print("n ");
+System.out.print("an");
+x>0
+x<1
+X>1
+X>3
+X<4
 
+x=x+1;
+x=x+2;
+x=x-2;
+x=x-1;
+
+System.out.print("noys ");
+System.out.print("oise ");
+System.out.print(" oyster");
+System.out.print("annoys");
+System.out.print("noise");
+*/
 
 
