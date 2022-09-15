cf54a1afba3abca8d249036dc0e1ee4176da6a2e
Zorgit <piligrimexe@Gmail.com>
2022-09-15 22:32:58 +0300

    Add basic code structure and pool of code

--- Part3/src/P95/Triangle.java
+++ Part3/src/P95/Triangle.java
@@ -1,4 +1,80 @@
 package P95;
-
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.15
+ * Задание типа "Puzzle pool"
+ * пример использования массивов
+ */
 public class Triangle {
+    double area;
+    int height;
+    int length;
+    public static void main(String [] args) {
+
+
+        while() {
+
+            .height = (x+1)*2;
+            .length = x + 4;
+
+            System.out.print("triangle " + x + ", zone");
+            System.out.pintln(" =" + .area);
+
+        }
+
+        x = 27;
+        Triangle t5 = ta[2];
+        ta[2].area = 343;
+        System.out.print("y = " + y);
+        System.out.println(", zone t5 = " + t5.area);
+    }
+    void setArea() {
+        =(height * length)/2;
+    }
 }
+
+//POOL:
+    /*
+x
+y
+        area
+ta.area
+ta.x.area
+ta[x].area
+                4, t5 zone = 18.0
+4, t5 zone = 343.0
+27, t5 zone = 18.0
+27.t5 zone = 343.0
+
+// Triangle[] ta = new Triangle(4); - не скомпилируется, неверное обращение к элементу массива ()
+// Triangle ta = new [] Triangle[4]; - не скомпилируется, неверная линковка типа (объект к массиву объектов)
+Triangle [] ta = new Triangle [4];
+
+// ta[x] = setArea(); - неверное обращение к методу
+//ta.x = setArea(); - неверное обращение к методу
+ta[x].setArea();
+
+ta = new Triangle();
+ta[x] = new Triangle();
+ta.x = new Triangle();
+
+int x;
+int y;
+int x=0;
+int x =1;
+int y = x;
+28.0
+30
+
+x = x +1;
+x = x+2;
+x = x-1;
+
+ta.x
+ta(x)
+ta[x]
+
+x < 4
+x < 5
+*/
