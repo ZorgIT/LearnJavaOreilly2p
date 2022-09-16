b38c148c51ed28776530e6b84010a7cd12accb6d
Zorgit <piligrimexe@Gmail.com>
2022-09-16 23:49:05 +0300

    Add solve code

--- Part4/src/p121/Puzzle4.java
+++ Part4/src/p121/Puzzle4.java
@@ -9,62 +9,76 @@ package p121;
 
 public class Puzzle4 {
     public static void main (String [] args) {
-
+        Puzzle4b [] obs= new Puzzle4b[6]; //
         int y = 1;
         int x = 0;
         int result = 0 ;
+        while (x < 6) {
+            obs[x] = new Puzzle4b();//
+            obs[x].ivar=y;//
+            y = y * 10;
+            x = x + 1; //
+        }
+        x=6;
+        while (x > 0) {
+            x = x - 1;
+            result = result + obs[x].doStuff(x);
+        }
+        System.out.println ("Result " + result);
 
     }
 
-    class {
+    static class Puzzle4b{
         int ivar;
-        _ _ doStuff(int ) {
+        public int doStuff (int factor) {
             if (ivar > 100) {
-                return
+                return ivar * factor;
             } else {
-                return
+                return ivar * (5 - factor);
             }
         }
     }
 
 }
 
+/*
 ivar = x;
 obs.ivar = x;
 obs[x].ivar=x;
-obs[x].ivar=y;
+
 
 Puzzle4 [] obs = new Puzzle4[6];
-Puzzle4b [] obs= new Puzzle4b[6];
+
 Puzlle4b [] obs = new Puzzle5[6];
 
 doStuff(x);
 obs.doStuff(x);
 obs[x].doStuff(factor);
-obs[x].doStuff(x);
 
-ivar
-factor
-public
+
+
+
+
 private
 
-ivar+factor;
-ivar * (2 + factor);
-ivar * (5 - factor);
-ivar * factor;
 
-x = x + 1;
-x = x - 1;
+ivar * (2 + factor);
+
+
+
+
+
 
 Puzzle4
-Puzzle4b
+
 Puzzle4b()
 
-int
+
 short
 
 obs[x] = new Puzzle4b (x);
 obs[] = new Puzzle4b();
-obs[x] = new Puzzle4b();
+
 obs = new Puzzle4b();
 
+*/
