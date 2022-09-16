f6e7cea131107c11218982ee4f8e44613b54c2a7
Zorgit <piligrimexe@Gmail.com>
2022-09-16 23:23:47 +0300

    Add example code with pool code.

--- Part4/src/p121/Puzzle4.java
+++ Part4/src/p121/Puzzle4.java
@@ -8,6 +8,63 @@ package p121;
  */
 
 public class Puzzle4 {
+    public static void main (String [] args) {
 
+        int y = 1;
+        int x = 0;
+        int result = 0 ;
+
+    }
+
+    class {
+        int ivar;
+        _ _ doStuff(int ) {
+            if (ivar > 100) {
+                return
+            } else {
+                return
+            }
+        }
+    }
 
 }
+
+ivar = x;
+obs.ivar = x;
+obs[x].ivar=x;
+obs[x].ivar=y;
+
+Puzzle4 [] obs = new Puzzle4[6];
+Puzzle4b [] obs= new Puzzle4b[6];
+Puzlle4b [] obs = new Puzzle5[6];
+
+doStuff(x);
+obs.doStuff(x);
+obs[x].doStuff(factor);
+obs[x].doStuff(x);
+
+ivar
+factor
+public
+private
+
+ivar+factor;
+ivar * (2 + factor);
+ivar * (5 - factor);
+ivar * factor;
+
+x = x + 1;
+x = x - 1;
+
+Puzzle4
+Puzzle4b
+Puzzle4b()
+
+int
+short
+
+obs[x] = new Puzzle4b (x);
+obs[] = new Puzzle4b();
+obs[x] = new Puzzle4b();
+obs = new Puzzle4b();
+
