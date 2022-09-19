359a1f87ed4c4dc5945832eba825b3fc108edb6c
Zorgit <piligrimexe@Gmail.com>
2022-09-19 22:51:19 +0300

    Add comments to code in SimpleDotCom.java

--- Part5/src/SimpleDotCom.java
+++ Part5/src/SimpleDotCom.java
@@ -9,20 +9,36 @@
 public class SimpleDotCom {
 
     public String checkYourself(String stringGuess) {
-       int guess = Integer.parseInt(stringGuess);
-       String result = "Мимо";
 
-       for (int cell : locationCells) {
-           if (guess == cell) {
-               result = "Попал";
-               numOfHits++;
-               break;
+        //Преобразование типа String в int
+        int guess = Integer.parseInt(stringGuess);
+
+        //Создание переменной для хранения результата который будем возвращать.
+        // По умолчанию присваиваем строковое значение "Мимо"
+        String result = "Мимо";
+
+       // повторяем с каждой ячейкой из массива locationCells (местоположение ячейки объекта).
+        for (int cell : locationCells) {
+           //Сравниваем ход пользователя с этим элементом (ячейкой) массива.
+            if (guess == cell) {
+                //Обнаруженно попадание.
+                result = "Попал";
+                // зафиксированно попадаение
+                numOfHits++;
+                //выход из цикла (принудительный).
+                break;
            } // end if
-       }// end for
+        }// end for
+
         if (numOfHits == locationCells.length) {
+            //После выхода из цикла проверяем не потоплен ли "сайт"
+            //При подтверждении - фиксируем результат.
             result = "Потопил";
         } // end if
+
+        //Вывод результа проверки пользователю - Мимо/Попал/Потопил.
         System.out.println(result);
+        //Возвращаем результат для последующей обработки в вызывающий метод.
         return result;
     }
 }
