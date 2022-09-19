174fb1025a7daf1e46be40c6a99669ce3260f70e
Zorgit <piligrimexe@Gmail.com>
2022-09-20 00:09:15 +0300

    Add basic code from p142

--- Part5/src/GameHelper.java
+++ Part5/src/GameHelper.java
@@ -1,2 +1,24 @@
+import  java.io.*;
+/**
+ *
+ * @author  none
+ * @version 2022.09.19
+ * Задание типа "Создание программ"
+ * Потопи сайт v 1.0.0
+ * Код "Готовый к употреблению"
+ */
 public class GameHelper {
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.println(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(
+                    new  InputStreamReader (System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+        return inputLine;
+    }
 }
