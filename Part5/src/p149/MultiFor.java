d685774ddfbde45850841a5975ba6d2a55f0ce7a
Zorgit <piligrimexe@Gmail.com>
2022-09-20 00:35:56 +0300

    Add solve code to MultiFor.java

--- Part5/src/p149/MultiFor.java
+++ Part5/src/p149/MultiFor.java
@@ -1,4 +1,22 @@
 package p149;
 
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.19
+ * Задание типа "Магнитики с кодом
+ */
+
 public class MultiFor {
+    public static void main(String[] args) {
+        for (int x = 0; x< 4; x++) {
+
+            for (int y = 4; y > 2; y --) {
+                System.out.println(x + " " + y);
+            }
+            if (x ==1 ){
+                x++;
+            }
+        }
+    }
 }
