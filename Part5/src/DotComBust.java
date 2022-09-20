b9730d032f23ec3dfe084cb1f03b1f944623ecd7
Zorgit <piligrimexe@Gmail.com>
2022-09-21 01:07:53 +0300

    Add basic code to DotComBust.java file

--- Part5/src/DotComBust.java
+++ Part5/src/DotComBust.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  *
  * @author  Stepan M
@@ -7,4 +9,74 @@
  * Создание модуля игры на основании псевдокода.
  */
 public class DotComBust {
+    //Объявление переменных
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComsList = new ArrayList<DotCom>(3);
+    private int numOfGuesses=0;
+
+    //Объявление методов
+
+    private void setUpGame() {
+       //Создает объекты DotCom и присвоеине им имен и адресов
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+
+        dotComsList.add(one);
+        dotComsList.add(two);
+        dotComsList.add(three);
+
+        System.out.println("Your point - sink three \"site\"");
+        System.out.println("Pets.com, eToys.com, Go2.com");
+        System.out.println("Get it in min turn");
+
+        for (DotCom dotComToSet:
+             dotComsList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+    private void startPlaying() {
+        //Запрашивает у пользователя ход и вызывает метод checkUserGuess()
+        while (!dotComsList.isEmpty()){
+            String userGuess = helper.getUserInput("input your point:");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+
+    }
+    void checkUserGuess(String userGuess) {
+        //Просматривает все остальные объекты DotCom и вызывает каждый объект DotCom метода CheckYourself().
+        numOfGuesses++;
+        String result = "Мимо";
+
+        for (DotCom dotComToTest:
+             dotComsList) {
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Got it!")) {
+                break;
+            }
+            if (result.equals("Sink")) {
+                dotComsList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        //Выводит на экран сообщение об успехах пользователя
+        //основываясь на том за сколько ходов тот потопил все объекты
+
+
+    }
+
+
+
+
+
+
 }
