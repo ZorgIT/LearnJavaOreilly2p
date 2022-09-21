eeba67efd9aa0f691f48651cbb1763f119003fed
Zorgit <piligrimexe@Gmail.com>
2022-09-21 21:31:46 +0300

    Add basic info in Doctor.java

--- Part7/src/p199/Doctor.java
+++ Part7/src/p199/Doctor.java
@@ -1,4 +1,14 @@
 package p199;
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.21
+ * ѕрактическа€ часть - наследование
+ */
 
 public class Doctor {
+    boolean worksAtHospital;
+    void treatPatient() {
+        //делаем чекап
+    }
 }
