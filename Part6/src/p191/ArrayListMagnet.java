222ee38b4f59cb618d44701ebf6c1b412b1c4e99
Zorgit <piligrimexe@Gmail.com>
2022-09-21 20:07:53 +0300

    Add solve code to ArrayListMagnet.java

--- Part6/src/p191/ArrayListMagnet.java
+++ Part6/src/p191/ArrayListMagnet.java
@@ -1,4 +1,5 @@
 package p191;
+import java.util.ArrayList;
 /**
  *
  * @author  Stepan M
@@ -7,4 +8,54 @@ package p191;
  */
 
 public class ArrayListMagnet {
+    public static void main (String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        a.add(0,"null");
+        a.add(1,"one");
+        a.add(2, "two");
+        a.add(3,"three");
+        printAL(a);
+
+        a.remove(2);
+        if (a.contains("three")) {
+            a.add("four");
+        }
+        printAL(a);
+        if (a.indexOf("four") != 4 ) {
+            a.add(4, "4.2");
+        }
+        printAL(a);
+
+        if (a.contains("two")) {
+            a.add("2.2");
+        }
+        printAL(a);
+
+    }
+
+    public static void printAL(ArrayList<String> a1) {
+        for (String element : a1) {
+            System.out.print(element + " ");
+        }
+        System.out.println(" ");
+    }
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
