2507885e3fc9edd7452b5d9f0bdcad308d400d55
Zorgit <piligrimexe@Gmail.com>
2022-09-22 22:00:33 +0300

    Rename NoName.java to Of76.java Add basic info Add solve code.

---
+++ Part8/src/Of76.java
@@ -0,0 +1,43 @@
+
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.22
+ * Задача типа "Pool puzzle"
+ * P262
+ */
+
+public class Of76 extends Clowns {
+    public static void main(String[] args) {
+        Nose [] i = new Nose[3];
+        i[0] = new Acts();
+        i[1] = new Clowns();
+        i[2] = new Of76();
+        for (int x = 0; x < 3; x++) {
+            System.out.println(i[x].iMethod()
+                    + " "
+                    +i[x].getClass());
+        }
+    }
+
+}
+
+interface Nose {
+    int iMethod();
+}
+
+abstract class Picasso implements Nose {
+    public int iMethod() {
+        return 7;
+    }
+}
+
+class Clowns extends  Picasso {}
+class Acts implements Nose {
+    public int iMethod(){
+        return 5;
+    }
+}
+
+
+
