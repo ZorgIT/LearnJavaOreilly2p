af2c9508b482cb4addd7a11664ed753c93b7dedd
Zorgit <piligrimexe@Gmail.com>
2022-09-24 23:47:45 +0300

    Add basic info in MiniMiniMusicApp.java file

--- Part11/src/p372/MiniMiniMusicApp.java
+++ Part11/src/p372/MiniMiniMusicApp.java
@@ -1,4 +1,18 @@
 package p372;
+import javax.sound.midi.*;
+
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.24
+ * Первое приложение для проигрывания звуков
+ * P372
+ */
 
 public class MiniMiniMusicApp {
-}
+
+
+    }
+
+
+}// end class
