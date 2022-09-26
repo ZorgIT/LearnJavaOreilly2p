f11f642d1954c2245fc3c5bd8f59646572166c55
Zorgit <piligrimexe@Gmail.com>
2022-09-27 00:55:27 +0300

    Add basic info in SimpleGui1.java file

--- Part12/src/p385/SimpleGui1.java
+++ Part12/src/p385/SimpleGui1.java
@@ -1,4 +1,15 @@
 package p385;
+import javax.swing.*;
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.27
+ * Первый графический интерефейс )
+ * P385
+ */
 
 public class SimpleGui1 {
+    public static void main (String [] args) {
+
+    }
 }
