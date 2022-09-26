693dcf28057ffb835b9dadbc9139c754240f9d9e
Zorgit <piligrimexe@Gmail.com>
2022-09-27 00:59:06 +0300

    Add basic code in SimpleGui1.java file

--- Part12/src/p385/SimpleGui1.java
+++ Part12/src/p385/SimpleGui1.java
@@ -11,5 +11,16 @@ import javax.swing.*;
 public class SimpleGui1 {
     public static void main (String [] args) {
 
+        //Create frame and button
+        JFrame frame = new JFrame();
+        JButton button = new JButton("click me"); // Передаем конструктору текст кнопки
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // при закрытии окна завершить программу
+
+        frame.getContentPane().add(button); //Добавляем кнопку на панель фрейма.
+
+        frame.setSize(300,300); // Размер фрейма
+
+        frame.setVisible(true); //Устанавливаем видимость фрейма
     }
 }
