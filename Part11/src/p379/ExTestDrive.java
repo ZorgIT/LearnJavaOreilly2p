34f902bbe447ce7173da58c1cc32285de10a05fe
Zorgit <piligrimexe@Gmail.com>
2022-09-27 00:28:59 +0300

    Add solve code in ExTestDrive.java

--- Part11/src/p379/ExTestDrive.java
+++ Part11/src/p379/ExTestDrive.java
@@ -8,4 +8,32 @@ package p379;
  */
 
 public class ExTestDrive {
+    public static void main(String[] args) {
+
+        String test = args[0];
+        System.out.print("t");
+        try {
+            doRisky(test);
+        } catch (MyEx e) {
+
+        } finally {
+            System.out.print("w");
+            System.out.print("s");
+        }
+    } //end main
+
+ static class MyEx extends Exception {
+
+ }
+   static void doRisky (String t) throws MyEx {
+        System.out.print("h");
+        if ("yes".equals(t)) {
+            System.out.print("a");
+            throw new MyEx();
+        }
+       System.out.print("r");
+       System.out.print("o");
+    }
+
+
 }
