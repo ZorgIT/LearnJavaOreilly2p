28227dea584ab3856b78b2dcad2586a512a71b3f
Zorgit <piligrimexe@Gmail.com>
2022-09-27 21:02:59 +0300

    Add basic code in SimpleGui1B.java file

--- Part12/src/p390/SimpleGui1B.java
+++ Part12/src/p390/SimpleGui1B.java
@@ -1,4 +1,6 @@
 package p390;
+import p385.SimpleGui1;
+
 import javax.swing.*;
 import java.awt.event.*;
 
@@ -10,6 +12,29 @@ import java.awt.event.*;
  * P390
  */
 
-public class SimpleGui1B {
+public class SimpleGui1B implements ActionListener { //SimpleGui1B реализуем ActionListener
+    JButton button;
+
+    public static  void main (String[] args) {
+        SimpleGui1B gui = new SimpleGui1B();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+        //Добавляем к списку слушателей (Передаваемый объект ДОЛЖЕН быть объектом класса реализуемого ActionListener
+        button.addActionListener(this); //Регистрируем заинтересованность в кнопке
+
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    public void actionPerformed (ActionEvent event) { //Реализуем метод actionPerformed() интерфейса ActionListener.
+        button.setText("I've been clicked!");
+    }
+
 
 }
