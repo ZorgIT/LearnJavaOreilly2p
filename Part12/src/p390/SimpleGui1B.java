e68b0e13d720582a1afbb1b46c2c74402de6d615
Zorgit <piligrimexe@Gmail.com>
2022-09-27 20:51:24 +0300

    Add basic info in SimpleGui1B.java file

--- Part12/src/p390/SimpleGui1B.java
+++ Part12/src/p390/SimpleGui1B.java
@@ -1,4 +1,15 @@
 package p390;
+import javax.swing.*;
+import java.awt.event.*;
+
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.27
+ * Графический интерфейс с ActionEvent кнопки
+ * P390
+ */
 
 public class SimpleGui1B {
+
 }
