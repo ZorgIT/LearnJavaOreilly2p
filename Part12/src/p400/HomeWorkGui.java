b72a93aad0f91f61b1173b4996c73732d1b2556d
Zorgit <piligrimexe@Gmail.com>
2022-09-28 20:52:38 +0300

    Add answer code. Create personal widget to redraw circle

--- Part12/src/p400/HomeWorkGui.java
+++ Part12/src/p400/HomeWorkGui.java
@@ -1,5 +1,9 @@
 package p400;
+import p390.SimpleGui1B;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -10,7 +14,8 @@ import javax.swing.*;
  * P390
  */
 
-public class HomeWorkGui {
+public class HomeWorkGui implements ActionListener {
+
 
     //Cоздать фрейм с двумя виджетами (панелью рисования и кнопкой).
     //Создать слушатель, который связывается с кнопкой.
@@ -22,5 +27,51 @@ public class HomeWorkGui {
     //Система ывазывает метод paintComponent() в контексте панели для отрисовки
 
     //Генерируется новый цвето по нажатию кнопки.
+    JButton button; //Ссылка на кнопку
+    JPanel testpannel; //Ссылка на панель
 
+    public static void main (String [] args) {
+        HomeWorkGui gui = new HomeWorkGui();
+        gui.go();
+    }
+
+    //процедура первичной прорисовки интерфейса
+    public void go() {
+        JFrame frame = new JFrame();
+        testpannel = new MyDrawPanel();
+        frame.getContentPane().add(testpannel);
+
+        button = new JButton("Тестовая кнопка");
+        button.addActionListener(this);
+
+        frame.setAlwaysOnTop(true);
+        frame.getContentPane().add(BorderLayout.SOUTH,button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600,300);
+        frame.setVisible(true);
+
+    }
+    public void actionPerformed (ActionEvent event) {
+        testpannel.repaint();
+    }
+
+    class MyDrawPanel  extends JPanel {
+        public void paintComponent(Graphics g) {
+
+            //Закрашивание всей панели черным
+            //Два первых аргумента x y координаты левого верхнего угла по отношению к панели где начнется рисование.
+            // два следующих аргумента - Ширина прямоугольника каку панели , высота как у панели.
+            g.fillRect(0,0,this.getWidth(), this.getHeight());
+
+            int red = (int) (Math.random() * 255);
+            int green = (int) (Math.random() * 255);
+            int blue = (int) (Math.random() * 255) ;
+
+            //Назначение сгенерированных случайных чисел
+            Color randomColor = new Color(red, green, blue);
+            g.setColor(randomColor);
+            g.fillOval(70, 70, 100, 100);
+
+        }
+    }
 }
