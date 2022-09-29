a7f3808dcf2c59f25269bf957214a0417c47da87
Zorgit <piligrimexe@Gmail.com>
2022-09-30 00:22:50 +0300

    Create MiniMusicPlayer2.java file

---
+++ Part12/src/p418/MiniMusicPlayer2.java
@@ -0,0 +1,16 @@
+package p418;
+
+import javax.sound.midi.*;
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.29 v1.0.0
+ * MusicPlayer
+ * Создание и тестирование вспомогательного метода
+ *
+ * P418-419
+ *
+ *
+ *
+*/
+
