8a03bba61c379988aded3b86c023f31d61fa57da
Zorgit <piligrimexe@Gmail.com>
2022-09-29 23:36:32 +0300

    Add clean procedure to get final animation

--- Part12/src/p414/SimpleAnimation.java
+++ Part12/src/p414/SimpleAnimation.java
@@ -29,6 +29,8 @@ public class SimpleAnimation {
         MyDrawPanel drawPanel = new MyDrawPanel();
 
         frame.getContentPane().add(drawPanel);
+        frame.setSize(300,300);
+        frame.setVisible(true);
         for (int i = 0; i < 130; i++) {
 
             x++;
@@ -45,6 +47,9 @@ public class SimpleAnimation {
     class MyDrawPanel extends JPanel {
 
         public void paintComponent (Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0,0,this.getWidth(),this.getHeight());
+
             g.setColor(Color.green);
             g.fillOval (x,y,40,40);
         }
