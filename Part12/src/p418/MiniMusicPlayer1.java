c2cb349e5c9854df11d1795ecf09de4529c6ccee
Zorgit <piligrimexe@Gmail.com>
2022-09-30 00:21:09 +0300

    Add test module

--- Part12/src/p418/MiniMusicPlayer1.java
+++ Part12/src/p418/MiniMusicPlayer1.java
@@ -26,10 +26,13 @@ public class MiniMusicPlayer1 {
 
             //Создаем группу событий, чтобы ноты продолжали поднимать (от ноты фортепиано 5 до ноты 61).
             for (int i = 5; i < 61; i+=4) {
+                //Вызываем метод makeEvent(), а затем добавляем его  в трек
+                // Событие включения 144, выключения 128
                 track.add(makeEvent(144,1,i,100,i));
                 track.add(makeEvent(128,1,i,100,i+2));
             } //end for
 
+            //Запуск трека
             sequencer.setSequence(seq);
             sequencer.setTempoInBPM(220);
             sequencer.start();
