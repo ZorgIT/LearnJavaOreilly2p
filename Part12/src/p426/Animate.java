15b6672c7117f0c591abb07e9d1687ac22481f27
Zorgit <piligrimexe@Gmail.com>
2022-10-01 00:17:36 +0300

    Add solve code

--- Part12/src/p426/Animate.java
+++ Part12/src/p426/Animate.java
@@ -1,4 +1,48 @@
 package p426;
 
+import  javax.swing.*;
+import java.awt.*;
+
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.09.30
+ * Задача тип "Puzzle pool"
+ *
+ * P426
+ */
+
 public class Animate {
+    int x = 1;
+    int y = 1;
+    public static void main (String[] args) {
+        Animate gui = new Animate();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame =new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        MyDrawP drawP = new MyDrawP();
+        frame.getContentPane().add(drawP);
+        frame.setSize(500, 270);
+        frame.setVisible(true);
+        for (int i = 0; i < 124; i++,x++,y++) {
+            x++;
+            drawP.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (Exception ex) {}
+        }
+    }
+
+    class MyDrawP extends JPanel {
+        public void paintComponent (Graphics g) {
+            g.setColor(Color.white);
+            g.fillRect(0,0,500,250);
+            g.setColor(Color.blue);
+            g.fillRect(x,y,500-x*2,250-y*2);
+        }
+    }
+
 }
