2b0b2678aa1f0b1ec5248268b35d889e48a04c3b
Zorgit <piligrimexe@Gmail.com>
2022-10-01 02:08:27 +0300

    Add text filed and panel2

--- Part13/src/p440/DispetcherLayoutLrn.java
+++ Part13/src/p440/DispetcherLayoutLrn.java
@@ -21,19 +21,33 @@ public class DispetcherLayoutLrn {
         JPanel panel = new JPanel();
         panel.setBackground(Color.darkGray);
 
+        //Изменяем диспетчер компоновки панели на новый экз. BoxLayout
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        //Конструктору диспетчера BoxLayout нужно знать, какие компоненты он размещает
+        // и какую  ось нужно использовать (указана Y_AXIS)
+
         JButton button = new JButton("Shok me");
         JButton buttonTwo = new JButton("bliss");
         JButton buttonThree = new JButton("huh?");
+
+        JPanel panel2 = new JPanel();
+        //JTextField field = new JTextField(20);
+        JTextField field = new JTextField(10);
+        field.setText("Тест замены");
+
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         panel.add(button);
         panel.add(buttonTwo);
         panel.add(buttonThree);
+        panel2.add(field);
+        frame.add(panel2);
 
         frame.add(panel);
 
         frame.getContentPane().add(BorderLayout.EAST, panel);
-        frame.setSize(250,200);
+        frame.getContentPane().add(BorderLayout.WEST, panel2);
+        frame.setSize(600,200);
         frame.setVisible(true);
     }
 
