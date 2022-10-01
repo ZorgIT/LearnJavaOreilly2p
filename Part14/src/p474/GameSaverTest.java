a50d89c3c75a6497af32efcaad38b8def57654d4
Zorgit <piligrimexe@Gmail.com>
2022-10-02 01:34:08 +0300

    Create GameSaverTest.java file Add basic information

---
+++ Part14/src/p474/GameSaverTest.java
@@ -0,0 +1,14 @@
+package p474;
+
+/**
+ *
+ *
+ * @author  Stepan M
+ * @version 2022.10.02
+ * пример сохранения и восстановление игровых персонажей
+ * P474-475
+ */
+
+public class GameSaverTest {
+
+}
