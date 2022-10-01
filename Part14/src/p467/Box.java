e68a2d4865b9a551ac022995683104e5a9dbeb5c
Zorgit <piligrimexe@Gmail.com>
2022-10-02 01:00:38 +0300

    Create Pond.java file

--- Part14/src/p467/Box.java
+++ Part14/src/p467/Box.java
@@ -1,4 +1,43 @@
 package p467;
+import java.io.*;
 
-public class Box {
+/**
+ *
+ * @author  Stepan M
+ * @version 2022.10.02
+ * Пример сериализации класса
+ * P467
+ */
+
+//Необходимо реализовать Serializable (нет методов, определять не нужно)
+public class Box implements Serializable{
+
+    private int width;
+    private int height;
+
+    public void setWidth (int w) {
+        width = w;
+    }
+
+    public void setHeight (int h) {
+        height = h;
+    }
+
+    public static void main (String [] args) {
+
+        Box myBox = new Box();
+        myBox.setWidth(50);
+        myBox.setHeight(20);
+
+        //Операции ввода\вывода могут вызывать исключения, необходим обработчик
+        try {
+            FileOutputStream fs = new FileOutputStream("foo.ser"); //подключиться к файлу или создать новый.
+            ObjectOutputStream os = new ObjectOutputStream(fs); //Связать ObjectOutputStream
+            // с потоком соединения, приказываем записать объект
+            os.writeObject(myBox);
+            os.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 }
---
+++ Part14/src/p468/Pond.java
@@ -0,0 +1,4 @@
+package p468;
+
+public class Pond {
+}
