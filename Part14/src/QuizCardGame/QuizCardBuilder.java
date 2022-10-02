e499e0d4fa59cfc93b0ac3ae1712ccac9f7195c9
Zorgit <piligrimexe@Gmail.com>
2022-10-03 00:58:38 +0300

    Create QuizCardBuilder.java file Add basic info

---
+++ Part14/src/QuizCardGame/QuizCardBuilder.java
@@ -0,0 +1,14 @@
+package QuizCardGame;
+
+/**
+ *
+ *
+ * @author  Stepan M
+ * @version 2022.10.03
+ * Модуль построения игровых карт
+ * "Готовый код"
+ * P480-481
+ */
+
+public class QuizCardBuilder {
+}
