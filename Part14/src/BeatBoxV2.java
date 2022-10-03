2164a804f0371c38bc6fbc74ef3f75c65c024bb5
Zorgit <piligrimexe@Gmail.com>
2022-10-04 00:44:15 +0300

    Add deserialization method in BeatBoxV2.java file

--- Part14/src/BeatBoxV2.java
+++ Part14/src/BeatBoxV2.java
@@ -3,9 +3,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.util.ArrayList;
 
 /**
@@ -218,4 +216,27 @@ public class BeatBoxV2 {
         }
     }
 
+    public class MyReadInListener implements ActionListener {
+        public void actionPerformed(ActionEvent a ) {
+            boolean[] checkboxState = null;
+            try {
+                FileInputStream fileIn = new FileInputStream(new File(Checkbox.ser));
+                ObjectInputStream is = new ObjectInputStream(fileIn);
+                checkboxState = (boolean[])  is.readObject();
+
+            } catch (Exception ex) {ex.printStackTrace();}
+
+            for (int i = 0; i < 256; i++) {
+                JCheckBox check = (JCheckBox) checkBoxList.get(i);
+                if (checkboxState[i]) {
+                    check.setSelected(true);
+                } else {
+                    check.setSelected(false);
+                }
+            }
+
+            sequencer.stop();
+            buildTrackAndStart();
+        }
+    }
 } // end BeatBox
