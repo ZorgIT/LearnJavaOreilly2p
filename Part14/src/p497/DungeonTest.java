620eadcdc3b2cf8eb8d64584de58f72c689799c5
Zorgit <piligrimexe@Gmail.com>
2022-10-04 22:22:57 +0300

    Create DungeonTest.java Add Basic info

---
+++ Part14/src/p497/DungeonTest.java
@@ -0,0 +1,15 @@
+package p497;
+
+
+import java.io.Serializable;
+
+/**
+ * @author  Stepan M
+ * @version 2022.10.04
+ * Задание типа "Магинты с кодом"
+ * P497
+ */
+public class DungeonTest {
+
+
+}
