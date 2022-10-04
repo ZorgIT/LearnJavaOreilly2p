5042aa4b43304f2e3857e6c7d2d964108411f163
Zorgit <piligrimexe@Gmail.com>
2022-10-05 01:17:53 +0300

    Add basic code to server part of DailyAdviceServer.java file

--- Part15/src/p511/DailyAdviceServer.java
+++ Part15/src/p511/DailyAdviceServer.java
@@ -1,6 +1,11 @@
 package p511;
 
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @author  Stepan M
  * @version 2022.10.05
@@ -15,4 +20,47 @@ package p511;
  */
 
 public class DailyAdviceServer {
+
+    String [] adviceList = {"Пробежать 20 км ", " Радоваться жинзи",
+            "Позвонить маме!", "жить", "Никому ничего не дложен )"};
+
+    public static void main (String[] args) {
+        DailyAdviceServer server = new DailyAdviceServer();
+        server.go();
+    }
+
+    public String getAdvice() {
+        int random = (int) (Math.random() * adviceList.length);
+        return adviceList[random];
+    }
+
+
+    public void go () {
+        try {
+            //ServerSocket позволяет приложению отслеживать клиентские запросы на порту 4242.
+            ServerSocket serverSock = new ServerSocket(4242);
+
+
+            //Постоянный цикл - ожидание клиентски подключений (и их обслуживание).
+            while (true) {
+
+                //Метод accept() блокирует приложение до тех пор,
+                // пока не поступит запрос, после чего возвращает сокет
+                // ( НА АНОНИМНОМ порту для взаимодействия с клиентом.)
+                Socket sock = serverSock.accept();
+
+                //берем установленное соединение и передаем его в PrinterWriter
+                PrintWriter writer = new PrintWriter(sock.getOutputStream());
+                String advice = getAdvice();
+                writer.println(advice);
+                writer.close();
+                //После завершения работы закрываем writer.
+                System.out.println(advice);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    } //end go() method.
 }
