2a60d58a955fae5cec2cd159f30388741cf3ae6c
Zorgit <piligrimexe@Gmail.com>
2022-10-05 23:29:04 +0300

    Add server code in VerySimpleChatServer.java file

--- Part15/src/p548/VerySimpleChatServer.java
+++ Part15/src/p548/VerySimpleChatServer.java
@@ -1,6 +1,8 @@
 package p548;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 /**
  * @author  Stepan M
@@ -14,4 +16,70 @@ import java.util.ArrayList;
 
 public class VerySimpleChatServer {
 
-}
+    ArrayList clientOutputStreams;
+    public static void main (String [] args) {
+        new VerySimpleChatServer().go();
+    }
+
+    public void go () {
+        clientOutputStreams = new ArrayList();
+        try {
+            ServerSocket serverSock = new ServerSocket(5000);
+
+            while (true ) {
+                Socket clientSocket = serverSock.accept();
+                PrintWriter writer = new PrintWriter(clientSocket.getOutputStream());
+                clientOutputStreams.add(writer);
+
+                Thread t = new Thread(new ClientHandler((clientSocket)));
+                t.start();
+                System.out.println("got a connection");
+            }
+
+        } catch (Exception ex ) { ex.printStackTrace();}
+    } // eng go ();
+
+    public void tellEveryone(String message) {
+
+        Iterator it = clientOutputStreams.iterator();
+        while (it.hasNext()) {
+            try {
+                PrintWriter writer = (PrintWriter) it.next();
+                writer.println(message);
+                writer.flush();
+            } catch (Exception ex) { ex.printStackTrace();}
+        } //end while
+    } //end tellEveryone
+
+    public class ClientHandler implements Runnable {
+        BufferedReader reader;
+        Socket sock;
+
+        public ClientHandler(Socket clientSocket) {
+            try {
+                sock = clientSocket;
+                InputStreamReader isReader = new InputStreamReader(sock.getInputStream());
+                reader = new BufferedReader(isReader);
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        public void run() {
+            String message;
+            try {
+                while ((message = reader.readLine()) != null) {
+                    System.out.println("read " + message);
+                    tellEveryone(message);
+                }
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        } // end run()
+    } //end ClientHandler class
+
+
+
+} // end VerySimpleChatServer
