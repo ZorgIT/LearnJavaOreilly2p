613df2d45504cb6bd31cf4b0ef9b25ae769ae380
Zorgit <piligrimexe@Gmail.com>
2022-10-05 23:15:58 +0300

    Create VerySimpleChatServer.java file

---
+++ Part15/src/p548/VerySimpleChatServer.java
@@ -0,0 +1,17 @@
+package p548;
+
+import java.util.ArrayList;
+
+/**
+ * @author  Stepan M
+ * @version 2022.10.05
+ * Задание типа "Готовый код"
+ * Серверная часть
+ *
+ *
+ * P516
+ */
+
+public class VerySimpleChatServer {
+
+}
