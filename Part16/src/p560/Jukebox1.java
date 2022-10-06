6c2c9dd40dd9b5b2a91300e271e5e513d9ab1102
Zorgit <piligrimexe@Gmail.com>
2022-10-06 21:46:46 +0300

    Add basic code in Jukebox1.file Add basic string list in SongList.txt to test

--- Part16/src/p560/Jukebox1.java
+++ Part16/src/p560/Jukebox1.java
@@ -1,5 +1,9 @@
 package p560;
 
+ipmrot java.util.*;
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * @author  Stepan M
  * @version 2022.10.06
@@ -9,4 +13,31 @@ package p560;
 
 public class Jukebox1 {
 
+    //Для храненеия наименоавний песен
+    ArrayList<String> songList = new ArrayList<>();
+
+    public static void main (String[] args){
+        new Jukebox1().go();
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception ex) {ex.printStackTrace();}
+    } //end getSong()
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        songList.add(tokens[0]);
+    }
 }
