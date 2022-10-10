9c759eb39793e187cd372bac4c0330a1c2f3a25b
Zorgit <piligrimexe@Gmail.com>
2022-10-10 22:29:47 +0300

    Create Basic servlet with html test page

--- Part18/src/pMyServletA/MyServletA.java
+++ Part18/src/pMyServletA/MyServletA.java
@@ -1,5 +1,9 @@
 package pMyServletA;
 
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
 
 /**
  *
@@ -9,5 +13,17 @@ package pMyServletA;
  */
 
 
-public class MyServletA {
+public class MyServletA extends HttpServlet {
+
+    public void doGet (HttpServletRequest request, HttpServletResponse response)
+        throws ServletExcetion, IOException {
+
+        response.setContentType("text/html");
+        PrinterWriter out = response.getWriter();
+        String message = "If you read this text - all work correctly";
+        out.println("<HTML><BODY>");
+        out.println("<H1" + message + "</H1>");
+        out.println("</BODY></HTML>");
+        out.close();
+    }
 }
